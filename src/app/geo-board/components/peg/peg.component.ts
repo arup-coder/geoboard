1389ab6103b485bf355985fc54ed7caa3895b376
Prosenjit <prosenjit.saha@capgemini.com>
2020-05-19 20:06:32 +0530

    Code Refactoring.

--- src/app/geo-board/components/peg/peg.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'peg',
-  templateUrl: './peg.component.html',
-  styleUrls: ['./peg.component.scss']
-})
-export class PegComponent { }
